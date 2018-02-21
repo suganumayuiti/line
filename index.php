90b8941dada5b9d62963834176cb68ffd00bbae3
菅沼雄一 <suga@Pro.local>
2018-02-22 01:00:31 +0900

    make it bette

--- index.php
+++ index.php
@@ -1,15 +1,6 @@
 <?php
-phpinfo();
-require_once __DIR__ . '/vendor/autoload.php';
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+require_once __DIR__. '/vendor/autoload.php';
 
-$bot = new \LINE\LINEBot($httpClient,['channelSecret'=> getenv('CHANNEL_SECRET')]);
-
-$signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
-
-$events = $bot->parseEventRequest(file_get_contents('php://input'),$signature);
-
-foreach($events as $evemt){
-    $bot -> replyText($event->getReplyToken(),'TextMessage');
-}
+$inputString = file_get_contents('php://input');
+error_log($inputString);
 ?>
---
+++ index1.php
@@ -0,0 +1,15 @@
+<?php
+phpinfo();
+require_once __DIR__ . '/vendor/autoload.php';
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+
+$bot = new \LINE\LINEBot($httpClient,['channelSecret'=> getenv('CHANNEL_SECRET')]);
+
+$signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+
+$events = $bot->parseEventRequest(file_get_contents('php://input'),$signature);
+
+foreach($events as $evemt){
+    $bot -> replyText($event->getReplyToken(),'TextMessage');
+}
+?>
