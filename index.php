663bc5f780a1a0d7102dd3dea0a71cf806ff48de
菅沼雄一 <suga@Pro.local>
2018-02-22 01:17:43 +0900

    make it better3

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ phpinfo();
 require_once __DIR__ . '/vendor/autoload.php';
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
 
-$bot = new \LINE\LINEBot($httpClient,['channelSecret'=> getenv('CHANNEL_SECRET')]);
+$bot = new \LINE\LINEBot($httpClient,['channelSecret' => getenv('CHANNEL_SECRET')]);
 
 $signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 
