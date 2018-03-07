cb795214f4f9fc44961555f3564d68b1fedecdba
菅沼雄一 <suga@Pro.local>
2018-03-07 12:56:41 +0900

    make it better

--- Procfile
+++ Procfile
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 web: vendor/bin/heroku-php-nginx -C nginx_app.conf
-=======
-web:vendor/bin/heroku-php-nginx
->>>>>>> heroku/master
--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
 
 //Composerでインストールしたライブラリを一括読み込み
 require_once __DIR__ . '/vendor/autoload.php';
@@ -149,21 +148,3 @@ function replyCarouselTemplate($bot,$replyToken,$alternativeText,$columnArray){
 }
 
 ?>
-=======
-phpinfo();
-require_once __DIR__ . '/vendor/autoload.php';
-
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
-
-$bot = new \LINE\LINEBot($httpClient,['channelSecret' => getenv('CHANNEL_SECRET')]);
-
-$signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
-
-$events = $bot->parseEventRequest(file_get_contents('php://input'),$signature);
-
-foreach($events as $event){
-    ChromePhp::error('errorログです');  
-    $bot->replyText($event->getReplyToken(),'TextMessage');
-}
-?>
->>>>>>> heroku/master
